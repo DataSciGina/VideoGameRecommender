49a06846d808a7207728ae6f8701a79459c1174c
Gina-DataScientist <fernandezagostina.ds@gmail.com>
2024-10-09 11:03:31 -0300

    ETL-steam_games completo
    - ETL de Steam Games completo.
    - ETL de User Items mejorado.
    - Archivo README.md bosquejado.
    - Primera función de lectura de archivos Parquet o CSV en archivo ./functions/EDA.

--- functions/ETL.py
+++ functions/ETL.py
@@ -9,14 +9,17 @@ def load_data(file_path: str):
     '''
     Carga el DataSet desde un archivo gzip y lo retorna como un DataFrame de Pandas.
     '''
-    arr = np.array([])
+    arr = np.array([]) # se crea array de numpy
 
+    # Se abre el archivo gzip y se lee línea por línea
     with gzip.open(file_path, 'rt', encoding='utf8') as file:
+        # Se intenta convertir cada línea en un diccionario y agregarlo al array
         try:
             for line in file.readlines():
                 arr = np.append(arr, ast.literal_eval(line))
-                
+        
         except ValueError:
+            # Se intenta leer el archivo como un JSON y agregarlo al array
             arr = pd.read_json(file_path, lines=True)
             return arr
         except Exception as e:
@@ -32,8 +35,12 @@ def normalize(df, col: str):
     Recibe un DataFrame de Pandas y el nombre de la columna a expandir y normalizar de dicho DataFrame.
     Devuelve un DataFrame normalizado con la columna expandida.
     '''
+    
+    # Expande la columna y resetea el índice para que los valores de la columna sean filas independientes
     df_exp = df.explode(col)
     df_exp.reset_index(drop=True, inplace=True)
+    
+    # Expande la columna normalizada y concatena el DataFrame original con la expandida
     df_normalized = json_normalize(df_exp[col])
     res = pd.concat([df_exp.drop(columns=[col]), df_normalized], axis=1)
     
